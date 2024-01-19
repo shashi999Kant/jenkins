b120d552c759486cb1c8c2dd3eb4b4f8c0135e3b
Shashi_kant_tiwari <skt972130@gmail.com>
2024-01-19 19:47:55 +0530

    first time committed

--- Hii.java
+++ Hii.java
@@ -4,8 +4,9 @@ public class Hii {
     
     public static void main(String[] args) {
         System.out.println("This is java program for jenkin testing purpose");
-        System.out.println("output from java program");
+        System.out.println("output from java program updated");
         System.out.println(new Date());
+        System.out.println("up[date 1 time]");
 
     }
 }
