af331d4fca7f9fde57eef53acbb88d7251ac09d9
Shashi_kant_tiwari <skt972130@gmail.com>
2024-01-19 19:41:20 +0530

    Your commit message

---
+++ Hii.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Hii {
+    
+    public static void main(String[] args) {
+        System.out.println("This is java program for jenkin testing purpose");
+        System.out.println("output from java program");
+        System.out.println(new Date());
+
+    }
+}
