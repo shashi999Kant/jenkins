d1a612f74264eb0d03806cca8386f655fddc52fc
Shashi_kant_tiwari <skt972130@gmail.com>
2024-01-25 15:17:39 +0530

    Description of changes

---
+++ NodeMain.js
@@ -0,0 +1,16 @@
+// Hanuman Chalisa text (you can replace it with the actual text)
+const hanumanChalisa = `
+    श्रीगुरु चरन सरोज रज, निज मनु मुकुरु सुधारि।
+    बरनउँ रघुबर बिमल जसु, जो दायकु फल चारि।।
+    ...
+    बाल समय रवि भक्षि लियो तब, तेन अहि भये बिभीषण।
+    गयौ सकल जगत भयो सुखारी, रामचरित मनस कहानी।।
+`;
+
+// Function to print Hanuman Chalisa to console
+function printHanumanChalisa() {
+    console.log(hanumanChalisa);
+}
+
+// Execute printing Hanuman Chalisa
+printHanumanChalisa();
